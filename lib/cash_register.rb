e29802c64caeb56f2bba6ac8fcc88c115a266341
Jeremiah Rodden <jrodden1@gmail.com>
2019-06-19 22:36:50 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
 
+class CashRegister
+   attr_accessor :total, :discount, :items, :last_transaction
+
+   def initialize(discount=0.0)
+      self.total = 0.0
+      self.discount = discount
+      self.items = []
+      self.last_transaction = []
+   end
+
+   def add_item(title, price, qty=1)
+      qty.times do
+         self.total += price.to_f
+         self.items << title
+      end
+      self.last_transaction << [title, price, qty]
+   end
+
+end  
